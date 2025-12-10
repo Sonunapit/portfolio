2f16417d944c4062461058463518b344485c4160
Sonu Kumar Napit <sonu714169@gmail.com>
2025-12-10 20:39:53 +0530

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,4 +33,4 @@ export const ThemeToggle = () => {
         {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}
         </button>
 }
-export const ThemeToggle = () => { ... }
+
